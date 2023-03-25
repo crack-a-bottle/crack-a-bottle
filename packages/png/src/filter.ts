7ba7963e3d100a015b7c7b6fefcd70d8259ad388
Justin Pridgen <justincpridgen672@gmail.com>
2023-03-25 19:26:04 -0400

    Obvious mistake I forgot to fucking fix #3

--- packages/png/src/filter.ts
+++ packages/png/src/filter.ts
@@ -22,47 +22,11 @@ export function reverse(data: Buffer, { width, height, channels, depth, interlac
         const empty = Buffer.alloc(byteWidth - 1);
 
         // A function to reverse the filter used on the scanline.
-        let unfilter: (x: number, a?: number, b?: number, c?: number) => number;
+        let unfilter: (a: number[], b: number, x: number) => number[];
         // For every scanline, do the following
         for (let y = 0; y < imageHeight; y++) {
             // The scanline to reverse the filter (at index 0) on.
             const scanline = imageData.subarray(y * byteWidth, (y + 1) * byteWidth);
-            // Make sure the filter is valid
-            assert.ok(scanline[0] < 5, "IDAT: Unrecognized filter type " + scanline[0]);
-            // Get the filter used on the scanline and determine what to do
-            switch (scanline[0]) {
-                case PNGFilter.NONE:    // Least complex filter, leave byte (X) as is
-                    unfilter = (x, a = 0, b = 0, c = 0) => x;
-                    break;
-                case PNGFilter.SUB:     // Add the unfiltered left byte (A) to the current filtered byte (X)
-                    unfilter = (x, a = 0, b = 0, c = 0) => x + a;
-                    break;
-                case PNGFilter.UP:      // Add the unfiltered upper byte (B) to the current filtered byte (X)
-                    unfilter = (x, a = 0, b = 0, c = 0) => x + b;
-                    break;
-                case PNGFilter.AVERAGE: // Add the floored mean of the unfiltered left (A) and upper bytes (B) to the current filtered byte (X)
-                    unfilter = (x, a = 0, b = 0, c = 0) => x + Math.floor((a + b) / 2);
-                    break;
-                case PNGFilter.PAETH:   // Most complex filter, add the byte that is closest to P (A + B - C) to the current filtered byte (X)
-                    unfilter = (x, a = 0, b = 0, c = 0) => {
-                        // I optimized this by subtracting from both sides lmao
-                        const paethA = Math.abs(b - c);         // P - A = A + B - C - A = B - C
-                        const paethB = Math.abs(a - c);         // P - B = A + B - C - B = A - C
-                        const paethC = Math.abs(a + b - 2 * c); // P - C = A + B - C - C = A + B - 2C
-
-                        switch (Math.min(paethA, paethB, paethC)) {
-                            case paethA:
-                                return x + a;
-                            case paethB:
-                                return x + b;
-                            case paethC:
-                            default:
-                                return x + c;
-                        }
-                    }
-                    break;
-            }
-
             // The sample distance, in bytes
             // If the bit depth is eight or more bits per sample, then compare channel-wise, byte-wise,
             // otherwise just compare byte-wise (Usually this means the filter method is NONE)
@@ -70,13 +34,59 @@ export function reverse(data: Buffer, { width, height, channels, depth, interlac
             // The previous unfiltered scanline, or an empty one if y is more than zero
             const previous = y > 0 ? scanlines[scanlines.length - 1] : empty;
 
+            // Make sure the filter is valid
+            assert.ok(scanline[0] < 5, "IDAT: Unrecognized filter type " + scanline[0]);
+            // Get the filter used on the scanline and determine what to do
+            switch (scanline[0]) {
+                case PNGFilter.NONE: // Least complex filter, leave byte as is
+                    unfilter = (a, b, x) => [...a.slice(0, x),
+                        b,
+                        ...a.slice(x + 1)];
+                    break;
+                case PNGFilter.SUB: // Add the unfiltered left byte to the current filtered byte
+                    unfilter = (a, b, x) => [...a.slice(0, x),
+                        b + (x >= distance ? a[x - distance] : 0),
+                        ...a.slice(x + 1)];
+                    break;
+                case PNGFilter.UP: // Add the unfiltered upper byte to the current filtered byte
+                    unfilter = (a, b, x) => [...a.slice(0, x),
+                        b + (y > 0 ? previous[x] : 0),
+                        ...a.slice(x + 1)];
+                    break;
+                case PNGFilter.AVERAGE: // Add the floored mean of the unfiltered left and upper bytes to the current filtered byte
+                    unfilter = (a, b, x) => [...a.slice(0, x),
+                        b + Math.floor(((x >= distance ? a[x - distance] : 0) + (y > 0 ? previous[x] : 0)) / 2),
+                        ...a.slice(x + 1)];
+                    break;
+                case PNGFilter.PAETH: // Most complex filter, add the byte that is closest to P (A + B - C) to the current filtered byte
+                    unfilter = (a, b, x) => {
+                        const A = x >= distance ? a[x - distance] : 0;
+                        const B = y > 0 ? previous[x] : 0;
+                        const C = x >= distance && y > 0 ? previous[x - distance] : 0;
+
+                        // I optimized this by subtracting from both sides lmao
+                        const paethA = Math.abs(B - C);         // P - A = A + B - C - A = B - C
+                        const paethB = Math.abs(A - C);         // P - B = A + B - C - B = A - C
+                        const paethC = Math.abs(A + B - 2 * C); // P - C = A + B - C - C = A + B - 2C
+
+                        switch (Math.min(paethA, paethB, paethC)) {
+                            case paethA:
+                                return [...a.slice(0, x), b + A, ...a.slice(x + 1)];
+                            case paethB:
+                                return [...a.slice(0, x), b + B, ...a.slice(x + 1)];
+                            case paethC:
+                            default:
+                                return [...a.slice(0, x), b + C, ...a.slice(x + 1)];
+                        }
+                    }
+                    break;
+                default: // This is an unreachable case, but it is needed for the function to be considered assigned
+                    unfilter = a => a;
+            }
+
             // Use TypedArray#reduce to view unfiltered scanline as it is created
             // Push to scanlines array when finished
-            scanlines.push(Buffer.from(scanline.subarray(1).reduce((a, b, x) => [
-                a.slice(0, x),
-                unfilter(b, x >= distance ? a[x - distance] : 0, y > 0 ? previous[x] : 0, x >= distance && y > 0 ? previous[x - distance] : 0),
-                a.slice(x + 1)
-            ], Array(byteWidth - 1))));
+            scanlines.push(Buffer.from(scanline.subarray(1).reduce(unfilter, Array(byteWidth - 1))));
         }
     }
 
