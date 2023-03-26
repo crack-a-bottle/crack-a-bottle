d8ee9a5a8f31632084ad876ec9cbaf6f34bb7c64
Justin Pridgen <justincpridgen672@gmail.com>
2023-03-26 18:16:46 -0400

    small bugfix

--- packages/png/src/index.ts
+++ packages/png/src/index.ts
@@ -52,17 +52,23 @@ export function png(data: Buffer, checkRedundancy: boolean = true) {
         switch (cType) {
             case "IHDR": { // Image header chunk
                 assert.strictEqual(i, 8, "IHDR: Header chunk cannot be after another chunk");
+
                 const width = chunk.readUInt32BE(0);
                 assert.ok(width > 0, "IHDR: Image width cannot be less than one");
+
                 const height = chunk.readUInt32BE(4);
                 assert.ok(height > 0, "IHDR: Image height cannot be less than one");
+
                 const depth = chunk[8];
                 assert.ok([1, 2, 4, 8, 16].includes(depth), "IHDR: Invalid bit depth " + depth);
+
                 const type = chunk[9];
                 assert.ok([0, 2, 3, 4, 6].includes(type), "IHDR: Invalid color type " + type);
                 const channels = 1 + 2 * (type & 1 ^ 1) * (type >> 1 & 1) + (type >> 2 & 1);
+
                 assert.strictEqual(chunk[10], 0, "IHDR: Unsupported compression method");
                 assert.strictEqual(chunk[11], 0, "IHDR: Unsupported filter method");
+
                 const interlace = chunk[12];
                 assert.ok(interlace < 2, "IHDR: Unsupported interlace method " + interlace);
 
@@ -88,27 +94,27 @@ export function png(data: Buffer, checkRedundancy: boolean = true) {
             case "IEND": { // Image ending chunk (After ALL chunks are parsed, parse the image data)
                 const { width, height } = json;
                 const { depth, interlace, channels } = info;
-                const bitWidth = width * channels;
+                const byteWidth = width * channels;
 
                 imageData = zlib.inflateSync(imageData, {
                     chunkSize: interlace ?
                         zlib.constants.Z_DEFAULT_CHUNK :
-                        Math.max((((bitWidth * depth + 7) >> 3) + 1) * height, zlib.constants.Z_MIN_CHUNK)
+                        Math.max(((byteWidth * depth + 7 >> 3) + 1) * height, zlib.constants.Z_MIN_CHUNK)
                 });
                 if (!imageData || !imageData.length) throw new Error("IDAT: Invalid inflate response");
                 else imageData = filter.reverse(imageData, { width, height, depth, channels, interlace });
                 const byteDepth = 8 / depth;
                 const bitmap = depth <= 8 ?
-                    imageData.toJSON().data.flatMap(x => util.fill(byteDepth, y => (x >> depth * y) % 2 ** depth).reverse()).slice() :
+                    imageData.toJSON().data.flatMap(x => util.fill(byteDepth, y => (x >> depth * y) % 2 ** depth).reverse()) :
                     imageData.toJSON().data.map((x, j) => j % 2 == 0 ? ((x << 8) | imageData[j + 1]) : NaN).filter(x => !Number.isNaN(x));
 
                 if (interlace) {
-                    const coords = adam7.coords(width, height).map(x => [x[0] * channels, x[1]]);
-                    json.data = util.groupArray(util.fill(bitWidth * height, x => {
-                        const j = coords.findIndex(y => y[0] == x % bitWidth && y[1] == Math.floor(x / bitWidth)) * channels;
-                        return j >= 0 ? bitmap.slice(j, j + channels) : util.fill(channels, () => NaN);
-                    }).flat(), bitWidth);
-                } else json.data = util.groupArray(bitmap, Math.ceil(bitWidth / byteDepth) * byteDepth).map(x => x.slice(0, bitWidth));
+                    const coords = adam7.coords(width, height);
+                    json.data = util.groupArray(util.fill(width * height, x => {
+                        const index = coords.findIndex(y => y[0] == x % width && y[1] == Math.floor(x / width)) * channels;
+                        return index >= 0 ? bitmap.slice(index, index + channels) : util.fill(channels, () => NaN);
+                    }).flat(), byteWidth);
+                } else json.data = util.groupArray(bitmap, Math.ceil(byteWidth / byteDepth) * byteDepth).map(x => x.slice(0, byteWidth));
                 break;
             }
         }
