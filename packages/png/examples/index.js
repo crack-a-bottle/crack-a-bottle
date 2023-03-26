48882711ba247f1e61d8f061c9cf4be53e5ddd56
Justin Pridgen <justincpridgen672@gmail.com>
2023-03-25 22:04:43 -0400

    the meatriding is crazy

--- packages/png/examples/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const { png } = require("../dist");
-
-const imagePath = path.resolve(__dirname, `${process.argv[2]}.png`);
-
-fs.writeFileSync(path.join(imagePath.replace(".png", ".json")), JSON.stringify(png(fs.readFileSync(imagePath)), null, 2));
