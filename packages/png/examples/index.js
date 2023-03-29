95b0562e1d45c31c5e5fa9e3f986d1e5d26d5667
Justin Pridgen <justincpridgen672@gmail.com>
2023-03-28 22:47:20 -0400

    almost forgot

--- packages/png/examples/index.js
+++ packages/png/examples/index.js
@@ -4,4 +4,4 @@ const { png } = require("../dist");
 const folder = path.join(__dirname, process.argv[2]);
 
 for (const file of fs.readdirSync(folder).filter(x => x.endsWith(".png")).map(x => x.slice(0, x.lastIndexOf("."))))
-    fs.writeFileSync(path.join(folder, file + ".json"), JSON.stringify(png(fs.readFileSync(path.join(folder, file + ".png")))));
+    fs.writeFileSync(path.join(folder, file + ".json"), JSON.stringify(png(fs.readFileSync(path.join(folder, file + ".png"))), null, 2));
