548b6a39b7353eaab4b04c8413bf64a9e0c432e2
Justin Pridgen <justincpridgen672@gmail.com>
2023-04-15 11:16:58 -0400

    fix 1

--- packages/bmp/src/index.ts
+++ packages/bmp/src/index.ts
@@ -4,4 +4,6 @@ export const SIGNATURE = Buffer.of(66, 77);
 
 export function bmp(data: Buffer) {
     assert.deepStrictEqual(SIGNATURE, data.subarray(0, 2), "Start signature not found");
+
+    return [[0, 0, 0, 255]];
 }
