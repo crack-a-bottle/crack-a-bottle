ab07a536f1488739343ec6c7eabce8869896ab78
Justin Pridgen <justincpridgen672@gmail.com>
2023-04-16 14:27:58 -0400

    how does one

--- packages/png/examples/index.js
+++ packages/png/examples/index.js
@@ -3,14 +3,20 @@ const path = require("path");
 const { png } = require("../dist");
 const folder = path.join(__dirname, process.argv[2]);
 
-for (const file of fs.readdirSync(folder).filter(x => x.endsWith(".png")).map(x => x.slice(0, x.lastIndexOf(".")))) {
-    console.log(file);
-    const img = png(fs.readFileSync(path.join(folder, file + ".png")));
-    const data = img.data;
-    img.data = [];
+function stringify(obj) {
+    const { data } = obj;
     let stringData = "[\n    ";
     for (let i = 0; i < data.length; i++) {
         stringData += JSON.stringify(data[i], null, 0.1).replaceAll("\n", " ") + (i < data.length - 1 ? ",\n    " : "\n  ]");
     }
-    fs.writeFileSync(path.join(folder, file + ".json"), JSON.stringify(img, null, 2).replace("\"data\": []", "\"data\": " + stringData));
+    return JSON.stringify({ ...obj, data: [] }, null, 2).replace("\"data\": []", "\"data\": " + stringData);
+}
+
+if (folder.endsWith(".png")) {
+    fs.writeFileSync(folder.slice(0, folder.lastIndexOf(".")) + ".json", stringify(png(fs.readFileSync(folder))));
+} else {
+    for (const file of fs.readdirSync(folder).filter(x => x.endsWith(".png")).map(x => path.join(folder, x.slice(0, x.lastIndexOf("."))))) {
+        console.log(file.split(path.sep).pop());
+        fs.writeFileSync(path.join(file + ".json"), stringify(png(fs.readFileSync(path.join(file + ".png")))));
+    }
 }
--- packages/qoi/examples/index.js
+++ packages/qoi/examples/index.js
@@ -2,14 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const { qoi } = require("../dist");
 
-for (const file of fs.readdirSync(__dirname).filter(x => x.endsWith(".qoi")).map(x => x.slice(0, x.lastIndexOf(".")))) {
-    console.log(file);
-    const img = qoi(fs.readFileSync(path.join(__dirname, file + ".qoi")));
-    const data = img.data;
-    img.data = [];
+function stringify(obj) {
+    const { data } = obj;
     let stringData = "[\n    ";
     for (let i = 0; i < data.length; i++) {
         stringData += JSON.stringify(data[i], null, 0.1).replaceAll("\n", " ") + (i < data.length - 1 ? ",\n    " : "\n  ]");
     }
-    fs.writeFileSync(path.join(__dirname, file + ".json"), JSON.stringify(img, null, 2).replace("\"data\": []", "\"data\": " + stringData));
+    return JSON.stringify({ ...obj, data: [] }, null, 2).replace("\"data\": []", "\"data\": " + stringData);
+}
+
+if (process.argv.length > 2 && process.argv[2].endsWith(".qoi")) {
+    const file = path.join(__dirname, process.argv[2].slice(0, process.argv[2].lastIndexOf(".")));
+    fs.writeFileSync(file + ".json", stringify(qoi(fs.readFileSync(file + ".qoi"))));
+} else {
+    for (const file of fs.readdirSync(__dirname).filter(x => x.endsWith(".qoi")).map(x => path.join(__dirname, x.slice(0, x.lastIndexOf("."))))) {
+        console.log(file.split(path.sep).pop());
+        fs.writeFileSync(file + ".json", stringify(qoi(fs.readFileSync(file + ".qoi"))));
+    }
 }
