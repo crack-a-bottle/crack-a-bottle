3305dd4971ccff310bec3fb2dad8ac3630b53580
Justin Pridgen <justincpridgen672@gmail.com>
2023-04-15 23:47:29 -0400

    found the mistake

--- packages/png/src/bits.ts
+++ packages/png/src/bits.ts
@@ -5,7 +5,7 @@ export = function bits(channels: number, depth: number) {
             return Math.ceil(width * bpp / 8);
         },
         extract(data: Buffer) {
-            const bitArray = [7, 6, 5, 4, 3, 2, 1, 0].slice(8 - 8 / depth).map((_, x) => depth * x);
+            const bitArray = [7, 6, 5, 4, 3, 2, 1, 0].slice(8 - 8 / depth).map(x => depth * x);
             const max = 2 ** depth - 1;
             return depth <= 8 ? data.reduce((a, x) => a.concat(bitArray.map(y => (x >> y) & max)), [] as number[]) :
                 data.reduce((a, x, i) => i % 2 == 0 ? a.concat(x << 8) : (a[a.length - 1] |= x, a), [] as number[]);
